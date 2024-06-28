baf43470bf6aad177e2fe20ac2623878b69f132c
Habiba113 <hummehabiba087@gmail.com>
2024-06-28 14:46:06 +0500

    Define display inventory

--- PROJECT_NO_1.py
+++ PROJECT_NO_1.py
@@ -3,14 +3,11 @@ Inventory = {}
 Total_sales = 0
 
 
-def change_price(name,new_price):
-        if name in Inventory:
-            Inventory[name]['price'] = new_price
-            print('price of ' + name + ' changed to $' +str(new_price))
-        else:
-            print(name + "not found in inventory")
- 
-     
+def display_inventory():
+        print('Current inventory')
+        for items, details in Inventory.items():
+            print(f'{items}: ${details['price']}*{details['count']}')
+            print(f'Total sales:${Total_sales}') 
             
             
                  
