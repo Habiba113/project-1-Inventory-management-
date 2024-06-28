bb41ad776657cf1ec9b0eb3af52ffbda26f26e4a
Habiba113 <hummehabiba087@gmail.com>
2024-06-28 14:44:44 +0500

    Define buy items

--- PROJECT_NO_1.py
+++ PROJECT_NO_1.py
@@ -3,10 +3,18 @@ Inventory = {}
 Total_sales = 0
 
 
-def add_items(name,price,count):
-        Inventory[name] = {'price':price,'count':count}
-        print('Item', name + " is added to inventory")
-  
+def buy_item(name,quantity):
+        if name in Inventory:
+            if Inventory[name]['count'] >= quantity:
+                Inventory[name]['count'] -=quantity
+                total_price = Inventory[name]['price'] * quantity
+                global Total_sales
+                Total_sales += total_price
+                print(f"{quantity} {name}(s) bough succesfully for ${total_price:.2f}.")
+            else:
+                print("Insufficient stock of '{}',".format(name))
+        else:
+            print("Item '{}' not found in invenory.".format(name))
  
      
             
